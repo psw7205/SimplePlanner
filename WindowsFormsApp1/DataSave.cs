0236274d5019087d2e9d3b0939329ccb900351f3
unknown <phsu2021@sju.ac.kr>
2019-05-30 11:31:58 +0900

    init

---
+++ WindowsFormsApp1/DataSave.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    [Serializable]
+    public class BindingClass
+    {
+        public string BoardName { get; set; }
+        public List<BindingTab> Tabs { get; set; }
+    }
+
+    [Serializable]
+    public class BindingTab
+    {
+        public string TabName { get; set; }
+        public List<BindingWork> Works { get; set; }
+    }
+
+    [Serializable]
+    public class BindingWork
+    {
+        public BindingWork(string _name, string _content)
+        {
+            WorkName = _name;
+            WorkContent = _content;
+        }
+
+        public string WorkName { get; set; }
+        public string WorkContent { get; set; }
+    }
+
+    class DataSave
+    {
+        private string Filename = @"./test.dat";
+        public void SerializeData(BindingClass data)
+        {
+            BinaryFormatter binFmt = new BinaryFormatter();
+            using (FileStream fs = new FileStream(Filename, FileMode.Create))
+            {
+                binFmt.Serialize(fs, data);
+            }
+        }
+
+        public BindingClass DeserializeData()
+        {
+            BindingClass p;
+            BinaryFormatter binFmt = new BinaryFormatter();
+
+            try
+            {
+                using (FileStream rdr = new FileStream(Filename, FileMode.Open))
+                {
+                    p = (BindingClass)binFmt.Deserialize(rdr);
+                }
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+            
+
+            return p;
+        }
+
+    }
+}
---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.editTap = new System.Windows.Forms.Button();
+            this.newTab = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.editTap);
+            this.panel1.Controls.Add(this.newTab);
+            this.panel1.Controls.Add(this.tabControl1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 426);
+            this.panel1.TabIndex = 1;
+            // 
+            // editTap
+            // 
+            this.editTap.Location = new System.Drawing.Point(285, 11);
+            this.editTap.Name = "editTap";
+            this.editTap.Size = new System.Drawing.Size(75, 23);
+            this.editTap.TabIndex = 2;
+            this.editTap.Text = "edit Tab";
+            this.editTap.UseVisualStyleBackColor = true;
+            this.editTap.Click += new System.EventHandler(this.editTap_Click);
+            // 
+            // newTab
+            // 
+            this.newTab.Location = new System.Drawing.Point(204, 11);
+            this.newTab.Name = "newTab";
+            this.newTab.Size = new System.Drawing.Size(75, 23);
+            this.newTab.TabIndex = 2;
+            this.newTab.Text = "new Tab";
+            this.newTab.UseVisualStyleBackColor = true;
+            this.newTab.Click += new System.EventHandler(this.newTab_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Location = new System.Drawing.Point(3, 62);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(773, 364);
+            this.tabControl1.TabIndex = 1;
+            this.tabControl1.Selected += new System.Windows.Forms.TabControlEventHandler(this.tabControl1_Selected);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button newTab;
+        private System.Windows.Forms.Button editTap;
+        private System.Windows.Forms.TabControl tabControl1;
+    }
+}
+
